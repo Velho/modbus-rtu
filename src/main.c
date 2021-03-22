3692174941f4393aecc8109ec08dc5624967ab80
Joel <joel.anttila@outlook.com>
2021-03-22 17:14:56 +0200

    Using the main as testing.

--- src/main.c
+++ src/main.c
@@ -49,37 +49,35 @@ void USART_init(void);
 void USART_write(char data);
 void delay_ms(int delay);
 
-int usart_sent = 0;
+void I2C_Init(void)
+{
 
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;	// Enable GPIOB clock PB8(D15)=SCL,PB9(D14)=SDA.
+    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN; // (??)
 
-/**
- * Modbus frame management interface.
- */
+    GPIOB->AFR[1] &= ~0xFF;     //PB8,PB9 I2C1 SCL, SDA. AFRH8 and AFRH9. clear
+    GPIOB->AFR[1] |= 0x44;      //GPIOx_AFRL p.189,AF4=I2C1(0100 BIN) p.177
+    GPIOB->MODER &= ~0xF0000;   //PB8 and PB9 clear
 
-#define MODBUS_MAX_APDU_SZ 8
+    GPIOB->MODER |= 0xA0000;    // Alternate function mode PB8,PB9
+    GPIOB->OTYPER |= 0x300;     // output open-drain. p.184
 
-typedef struct mb_frame {
-	uint8_t saddr;
-	uint8_t func;
-	uint16_t address;
-	uint16_t quantity;
-	uint16_t crc;
-} mb_f_t;
+    // Driver circuit includes the pull-up resistors.
+    GPIOB->PUPDR &= ~0xF0000;   // No pull-up resistors for PB8 and PB9 p.185
 
-typedef struct modbus_buffer {
-	uint8_t buffer[80];
-} mb_buf_t;
-
-typedef struct modbus_com {
-	uint8_t buffer[80];
-	uint16_t size;
-	int completed;
-	int rx_handled;
-
-	mb_f_t frames[5];
-} mb_com_t;
-
-static mb_com_t com = { 0 };
+    /**
+     * Following is the required sequence in master mode.
+     * - Program the peripheral input clock in I2C_CR2 Register in
+     * 	 order generate correct timings.
+     * - Configure the clock control registers.
+     * - Configure the rise time register.
+     * - Program the I2C_CR1 register to enable the peripheral.
+     * - Set the START bit in the I2C_CR1 register to generate a Start condition (During the init or write?).
+     * Peripheral input clock frequency must be at least:
+     * 	- 2 MHz in SM mode
+     *  - 4 MHz in FM mode
+     */
+}
 
 /**
  **===========================================================================
@@ -88,59 +86,96 @@ static mb_com_t com = { 0 };
  **
  **===========================================================================
  */
-int main(void) {
-	/**
-	 *  IMPORTANT NOTE!
-	 *  See the <system_*.c> file and how/if the SystemInit() function updates
-	 *  SCB->VTOR register. Sometimes the symbol VECT_TAB_SRAM needs to be defined
-	 *  when building the project if code has been located to RAM and interrupts
-	 *  are used. Otherwise the interrupt table located in flash will be used.
-	 *  E.g.  SCB->VTOR = 0x20000000;
-	 */
+int main(void)
+{
+    /**
+     *  IMPORTANT NOTE!
+     *  See the <system_*.c> file and how/if the SystemInit() function updates
+     *  SCB->VTOR register. Sometimes the symbol VECT_TAB_SRAM needs to be defined
+     *  when building the project if code has been located to RAM and interrupts
+     *  are used. Otherwise the interrupt table located in flash will be used.
+     *  E.g.  SCB->VTOR = 0x20000000;
+     */
 
-	__disable_irq();
-	USART_init();
+    __disable_irq();
+    USART_init();
 
-	/* Configure the system clock to 32 MHz and update SystemCoreClock */
-	SetSysClock();
-	SystemCoreClockUpdate();
+    /* Configure the system clock to 32 MHz and update SystemCoreClock */
+    SetSysClock();
+    SystemCoreClockUpdate();
 
-	/* TODO - Add your application code here */
+    /* TODO - Add your application code here */
 
-	NVIC_EnableIRQ(USART2_IRQn);
-	__enable_irq();
+    NVIC_EnableIRQ(USART2_IRQn);
+    __enable_irq();
 
-	RCC->AHBENR |= 1; // Enable ABH bus clock.
+    RCC->AHBENR |= 1; // Enable ABH bus clock.
+    RCC->APB2ENR |= 0x00000200;
 
-	// Blink out LD5.
-	GPIOA->MODER &= ~0x00000C00;	//clear (input reset state for PA5). p184
-	GPIOA->MODER |= 0x400; 			//GPIOA pin 5 to output. p184
-	GPIOA->ODR ^= 0x20;				//0010 0000 xor bit 5. p186
-	delay_ms(1000);
-	GPIOA->ODR ^= 0x20;				//0010 0000 xor bit 5. p186
-	delay_ms(1000);
+    GPIOA->MODER |= 0x3; // 11
+    // ADC setup.
+    ADC1->SQR5 = 0; // conversion sequence starts at ch0
+    // ADC1->CR2 = 0;			// bit 1=0: Single conversion mode, bit 11=0 align right
+    ADC1->SMPR3 = 7;		  // 384 cycles sampling time for channel 0 (longest)
+    ADC1->CR1 &= ~0x03000000; // resolution 12-bit
+    ADC1->CR2 |= 0x1;		  // bit 0, ADC on/off (1=on, 0=off)
 
-	/* Infinite loop */
-	while (1) {
+    // Blink out LD5 as startup indicator.
+    GPIOA->MODER &= ~0x00000C00; //clear (input reset state for PA5). p184
+    GPIOA->MODER |= 0x400;		 //GPIOA pin 5 to output. p184
+    GPIOA->ODR ^= 0x20;			 //0010 0000 xor bit 5. p186
+    delay_ms(1000);
+    GPIOA->ODR ^= 0x20; //0010 0000 xor bit 5. p186
+    delay_ms(1000);
 
-		if (com.completed) {
-			// Copy the memory from buffer to frames array.
-			memcpy(/*to*/&com.frames[0], /*from */com.buffer, sizeof(mb_f_t));
+    // BME280_init(); // Initializes the sensor.
 
-			com.rx_handled = 1; // Just a control flag.
-			com.size = 0;
-			com.completed = 0;
-		}
+    int result = 0;
+    char buf[100];
 
-	}
-	return 0;
+    /* Infinite loop */
+    while (1)
+    {
+
+        // Modbus event loop.
+        // Handle the modbus packets out of the communication.
+
+        // Read sensor.
+        // Process sensor.
+        // Prepare sensor packet.
+
+        ADC1->CR2 |= 0x40000000; //start conversion
+        while (!(ADC1->SR & 0x2))
+            ; //wait for conversion complete
+
+        result = ADC1->DR; //read conversion result
+
+        sprintf(buf, "%d", result);
+
+        int len = 0;
+        while (buf[len] != '\0')
+        {
+            len++;
+
+            for (int i = 0; i < len; i++)
+            {
+                USART_write(buf[i]);
+            }
+
+            USART_write('\n');
+            USART_write('\r');
+            delay_ms(1000);
+        }
+    }
+    return 0;
 }
 
-void delay_ms(int delay) {
-	int i = 0;
-	for (; delay > 0; delay--)
-		for (i = 0; i < 2460; i++)
-			; 	//measured with oscilloscope
+void delay_ms(int delay)
+{
+    int i = 0;
+    for (; delay > 0; delay--)
+        for (i = 0; i < 2460; i++)
+            ; //measured with oscilloscope
 }
 
 /**
@@ -151,108 +186,115 @@ void delay_ms(int delay) {
  * @param  None
  * @retval None
  */
-#define HSI_STARTUP_TIMEOUT   ((uint16_t)0x0500) /* Time out for HSI start up */
+#define HSI_STARTUP_TIMEOUT ((uint16_t)0x0500) /* Time out for HSI start up */
 
-void SetSysClock(void) {
-	__IO uint32_t StartUpCounter = 0, HSIStatus = 0;
+void SetSysClock(void)
+{
+    __IO uint32_t StartUpCounter = 0, HSIStatus = 0;
 
-	/* Enable HSI */
-	RCC->CR |= ((uint32_t) RCC_CR_HSION);
+    /* Enable HSI */
+    RCC->CR |= ((uint32_t)RCC_CR_HSION);
 
-	/* Wait till HSI is ready and if Time out is reached exit */
-	do {
-		HSIStatus = RCC->CR & RCC_CR_HSIRDY;
-	} while ((HSIStatus == 0) && (StartUpCounter != HSI_STARTUP_TIMEOUT));
+    /* Wait till HSI is ready and if Time out is reached exit */
+    do
+    {
+        HSIStatus = RCC->CR & RCC_CR_HSIRDY;
+    } while ((HSIStatus == 0) && (StartUpCounter != HSI_STARTUP_TIMEOUT));
 
-	if ((RCC->CR & RCC_CR_HSIRDY) != RESET) {
-		HSIStatus = (uint32_t) 0x01;
-	} else {
-		HSIStatus = (uint32_t) 0x00;
-	}
+    if ((RCC->CR & RCC_CR_HSIRDY) != RESET)
+    {
+        HSIStatus = (uint32_t)0x01;
+    }
+    else
+    {
+        HSIStatus = (uint32_t)0x00;
+    }
 
-	if (HSIStatus == (uint32_t) 0x01) {
-		/*  PLL configuration: PLLCLK = (HSI * 6)/3 = 32 MHz */
-		RCC->CFGR &= (uint32_t) ((uint32_t) ~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLMUL
-				| RCC_CFGR_PLLDIV));
-		RCC->CFGR |= (uint32_t) (RCC_CFGR_PLLSRC_HSI | RCC_CFGR_PLLMUL4
-				| RCC_CFGR_PLLDIV2);
-	}
+    if (HSIStatus == (uint32_t)0x01)
+    {
+        /*  PLL configuration: PLLCLK = (HSI * 6)/3 = 32 MHz */
+        RCC->CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLMUL | RCC_CFGR_PLLDIV));
+        RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSI | RCC_CFGR_PLLMUL4 | RCC_CFGR_PLLDIV2);
+    }
 
-	else {
-		/* If HSI fails to start-up, the application will have wrong clock
-		 configuration. User can add here some code to deal with this error */
-	}
+    else
+    {
+        /* If HSI fails to start-up, the application will have wrong clock
+         configuration. User can add here some code to deal with this error */
+    }
 
-	/* Enable 64-bit access */
-	FLASH->ACR |= FLASH_ACR_ACC64;
+    /* Enable 64-bit access */
+    FLASH->ACR |= FLASH_ACR_ACC64;
 
-	/* Enable Prefetch Buffer */
-	FLASH->ACR |= FLASH_ACR_PRFTEN;
+    /* Enable Prefetch Buffer */
+    FLASH->ACR |= FLASH_ACR_PRFTEN;
 
-	/* Flash 1 wait state */
-	FLASH->ACR |= FLASH_ACR_LATENCY;
+    /* Flash 1 wait state */
+    FLASH->ACR |= FLASH_ACR_LATENCY;
 
-	/* Power enable */
-	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+    /* Power enable */
+    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
 
-	/* Select the Voltage Range 1 (1.8 V) */
-	PWR->CR = PWR_CR_VOS_0;
+    /* Select the Voltage Range 1 (1.8 V) */
+    PWR->CR = PWR_CR_VOS_0;
 
-	/* Wait Until the Voltage Regulator is ready */
-	while ((PWR->CSR & PWR_CSR_VOSF) != RESET)
-		;
+    /* Wait Until the Voltage Regulator is ready */
+    while ((PWR->CSR & PWR_CSR_VOSF) != RESET)
+        ;
 
-	/* HCLK = SYSCLK /1*/
-	RCC->CFGR |= (uint32_t) RCC_CFGR_HPRE_DIV1;
+    /* HCLK = SYSCLK /1*/
+    RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
 
-	/* PCLK2 = HCLK /1*/
-	RCC->CFGR |= (uint32_t) RCC_CFGR_PPRE2_DIV1;
+    /* PCLK2 = HCLK /1*/
+    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
 
-	/* PCLK1 = HCLK /1*/
-	RCC->CFGR |= (uint32_t) RCC_CFGR_PPRE1_DIV1;
+    /* PCLK1 = HCLK /1*/
+    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV1;
 
-	/* Enable PLL */
-	RCC->CR |= RCC_CR_PLLON;
+    /* Enable PLL */
+    RCC->CR |= RCC_CR_PLLON;
 
-	/* Wait till PLL is ready */
-	while ((RCC->CR & RCC_CR_PLLRDY) == 0)
-		;
+    /* Wait till PLL is ready */
+    while ((RCC->CR & RCC_CR_PLLRDY) == 0)
+        ;
 
-	/* Select PLL as system clock source */
-	RCC->CFGR &= (uint32_t) ((uint32_t) ~(RCC_CFGR_SW));
-	RCC->CFGR |= (uint32_t) RCC_CFGR_SW_PLL;
+    /* Select PLL as system clock source */
+    RCC->CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_SW));
+    RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
 
-	/* Wait till PLL is used as system clock source */
-	while ((RCC->CFGR & (uint32_t) RCC_CFGR_SWS) != (uint32_t) RCC_CFGR_SWS_PLL)
-		;
+    /* Wait till PLL is used as system clock source */
+    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)RCC_CFGR_SWS_PLL)
+        ;
 }
 
+void USART_init(void)
+{
+    RCC->APB1ENR |= 0x00020000;	 //set bit 17 (USART2 EN)
+    RCC->AHBENR |= 0x00000001;	 //enable GPIOA port clock bit 0 (GPIOA EN)
+    GPIOA->AFR[0] = 0x00000700;	 //GPIOx_AFRL p.188,AF7 p.177
+    GPIOA->AFR[0] |= 0x00007000; //GPIOx_AFRL p.188,AF7 p.177
+    GPIOA->MODER |= 0x00000020;	 //MODER2=PA2(TX) to mode 10=alternate function mode. p184
+    GPIOA->MODER |= 0x00000080;	 //MODER2=PA3(RX) to mode 10=alternate function mode. p184
 
-void USART_init(void) {
-	RCC->APB1ENR|=0x00020000; 	//set bit 17 (USART2 EN)
-	RCC->AHBENR|=0x00000001; 	//enable GPIOA port clock bit 0 (GPIOA EN)
-	GPIOA->AFR[0]=0x00000700;	//GPIOx_AFRL p.188,AF7 p.177
-	GPIOA->AFR[0]|=0x00007000;	//GPIOx_AFRL p.188,AF7 p.177
-	GPIOA->MODER|=0x00000020; 	//MODER2=PA2(TX) to mode 10=alternate function mode. p184
-	GPIOA->MODER|=0x00000080; 	//MODER2=PA3(RX) to mode 10=alternate function mode. p184
+    // Configure USART2.
+    USART2->BRR = 0x00000116; // 115200 BAUD and crystal 32MHz. p710, D05
 
-	// Configure USART2.
-	USART2->BRR = 0x00000116;	// 115200 BAUD and crystal 32MHz. p710, D05
+    USART2->CR1 = 0x00000008;  // TE bit. p739-740. Enable transmit
+    USART2->CR1 |= 0x00000004; // RE bit. p739-740. Enable receiver
+    USART2->CR1 |= 0x00002000; // UE bit. p739-740. Uart enable
 
-	USART2->CR1 = 0x00000008;	// TE bit. p739-740. Enable transmit
-	USART2->CR1 |= 0x00000004;	// RE bit. p739-740. Enable receiver
-	USART2->CR1 |= 0x00002000;	// UE bit. p739-740. Uart enable
+    // USART2->CR1 |= (1 << 6); 	// TC : Transmission complete.
+    // USART2->CR1 |= 0x80; 		// TXE : Transmit data register empty.
+    // USART2->CR1 |= 0x0020;		// RXNE bit : Read data register not empty
 
-	// USART2->CR1 |= (1 << 6); 	// TC : Transmission complete.
-	// USART2->CR1 |= 0x80; 		// TXE : Transmit data register empty.
-	// USART2->CR1 |= 0x0020;		// RXNE bit : Read data register not empty
-
-	// See page, 739 (CR1).
-	USART2->CR1 |= USART_CR1_RXNEIE;
+    // See page, 739 (CR1).
+    USART2->CR1 |= USART_CR1_RXNEIE;
 }
 
-void USART_write(char data) {
-	// Wait for TX buffer.
-	while (!(USART2->SR & 0x0080));
-	USART2->DR = (data);
+void USART_write(char data)
+{
+    // Wait for TX buffer.
+    while (!(USART2->SR & USART_CR1_TCIE))
+        ;
+    USART2->DR = (data);
 }
